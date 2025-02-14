0b1620dfb46c30e3bd89b82cde27fd7c09b1b7aa
wandy <47462838+wandyseven@users.noreply.github.com>
2025-02-14 17:23:39 +0100

    FIX SCSS

--- coach_p_website/src/pages/Home.jsx
+++ coach_p_website/src/pages/Home.jsx
@@ -4,12 +4,12 @@ import './home.scss'
 
 const Home = () => {
   return (
-    <div class="container ">
-      <div class="row  gap-3">
-        <div class=" rounded bloc-t">Column 7 </div>
-        <div class=" rounded bloc-t">Column 4</div>
-        <div class=" rounded bloc-b">Column 7 </div>
-        <div class=" rounded bloc-b">Column 4</div>
+    <div className="container ">
+      <div className="row  gap-3">
+        <div className=" rounded bloc-t">Column 7 </div>
+        <div className=" rounded bloc-t">Column 4</div>
+        <div className=" rounded bloc-b">Column 7 </div>
+        <div className=" rounded bloc-b">Column 4</div>
        
       </div>
     </div>
